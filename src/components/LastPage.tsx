ab900b6bda17d68af27c5f0d4d3b93af8dd5f61a
maksikarv-sys <maksikarv@gmail.com>
2026-02-13 23:25:18 +0000

    Update LastPage.tsx
    changed last note

--- src/components/LastPage.tsx
+++ src/components/LastPage.tsx
@@ -4,10 +4,24 @@ const LastPage = () => {
     <div className="flex flex-col items-center gap-8 p-4 text-center">
       <h2 className="text-3xl font-bold text-primary">With All My Love</h2>
       <p className="text-lg max-w-2xl">
-        Thank you for being with me, for turning ordinary moments into
-        unforgettable memories, and for lighting up my world every day. Your
-        love fills my heart and inspires me to be the best version of myself.
-        I’m forever grateful to have you by my side. With love - David
+       We met on our first day at uni, two strangers sharing a room and somehow i found my soulmate. 
+        From that day on you've been my constant; my safe space, my laughter at 2 am while 
+        talking about how shit the world is....my person.
+        
+      I don't think you even realise how strong you are. I've watched you work so unbelievably hard
+        for the life you want, even on days when it would've been easier to give up. Your resilience
+        insipires me more than you know. You have the biggest dreams, and i promise i will be right there 
+        - cheering the loudest when you achieve every single one of them. 
+
+      I hope i get to see you fall in love one day i the soft, magical way you deserve. I hope your mornings are always 
+        filled with good coffee, your bag always has your favourite lip gloss, and your life filled 
+        with cherry tomato pasta nights and shared bowls of nachos loaded with WHOLE jalapeños that i secretly 
+        hope you dont share with your husband (kidding...maybe not) 
+
+      Thank you for being my chosen family. Thank you for finding me that first day. I will always be grateful 
+        that out of everyone in the worl...i found you, and i hope i keep finding you. 
+
+        Love, Your darling 
       </p>
       <CuteHeart />
     </div>
